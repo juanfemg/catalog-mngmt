e3bb4cad48ae969182ec6d433253422e2a1e9571
Juan Felipe Mosquera <juanmosquera06@gmail.com>
2024-05-18 22:05:28 -0500

    initial commit

---
+++ src/main/java/co/com/stockap/catalog/CatalogMngmtApplication.java
@@ -0,0 +1,13 @@
+package co.com.stockap.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMngmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogMngmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/stockap/catalog/CatalogMngmtApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.stockap.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMngmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
